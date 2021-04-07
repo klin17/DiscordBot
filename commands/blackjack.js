0abcb16e03c2b417d2589e78c40e687a5561e2cc
K Lin <emailklin@gmail.com>
2021-04-06 21:34:41 -0400

    blackjack betting and points

--- commands/blackjack.js
+++ commands/blackjack.js
@@ -4,7 +4,8 @@ class CardPlayer {
 		this.name = name;
 		this.hand = [];
         this.user = user;
-        this.points = 0;
+        this.points = 100;
+        this.bet = 0;
 	}
 
 	receiveCard(card) {
@@ -27,8 +28,13 @@ class CardPlayer {
 		return result;
 	}
 
-    sumHand(high) {
-        return this.hand.reduce((acc, e) => acc + cardValue(e, high), 0);
+    sumHand() {
+        const high = this.hand.reduce((acc, e) => acc + cardValue(e, true), 0);
+        const low = this.hand.reduce((acc, e) => acc + cardValue(e, false), 0);
+        if(high > 21) {
+            return low;
+        }
+        return high;
     }
 }
 
@@ -38,15 +44,8 @@ function cardValue(cardString, high=true) {
         switch (cardString.charAt(0)) {
             case 'A':
                 return high ? 11 : 1;
-            case 'J':
-                return 11;
-            case 'Q':
-                return 12;
-            case 'K':
-                return 13;
             default:
-                console.warn("unknown cardstring value");
-                return undefined;
+                return 10;
         }
     }
     return n;
@@ -62,12 +61,14 @@ const initialDeck = [
 class Blackjack {
 	constructor(channel, players) {
         this.channel = channel;
+        players.forEach(p => p.hand = []);
 		this.players = players;
 		this.deck = [...initialDeck];
 		this.discard = [];
         this.turn = 0;
         this.bust = [];
         this.finished = false;
+        this.numBets = 0;
 	}
 
 	shuffle() {
@@ -103,49 +104,62 @@ class Blackjack {
 	showCards() {
 		for(let p of this.players) {
             let res = p.hand.length > 0 ? p.hand.join(" ") : "no cards";
-			this.channel.send(p.name + ": " + res);
+			this.channel.send(p.name + ": " + res +" - " + p.sumHand().toString());
 		}
 	}
 
 	dealInitial() {
 		this.dealOne(); this.dealOne();
+        const naturals = this.players.filter(p => p.sumHand() == 21);
         this.showCards();
+        if(naturals.length > 0) {
+            naturals.forEach(p => this.channel.send(p.name + " has a blackjack!"));
+            this.finished = true;
+        } else {
+            this.sayTurn();
+        }
 	}
 
     curPlayer() {
         return this.players[this.turn];
+        // if undefined then all players played their turn
     }
 
     sayTurn() {
-        this.channel.send("It's " + curGame.curPlayer().name + "'s turn");
+        this.channel.send("It's " + this.curPlayer().name + "'s turn");
     }
 
     hit() {
         this.curPlayer().receiveCard(this.topCard());
         let res = this.curPlayer().hand.length > 0 ? this.curPlayer().hand.join(" ") : "no cards";
-		this.channel.send(this.curPlayer().name + ": " + res);
+		this.channel.send(this.curPlayer().name + ": " + res + " - " + this.curPlayer().sumHand());
 
         // check if over
-        const highVal = this.curPlayer().sumHand(true);
-        if(highVal > 21) {
-            const lowVal = this.curPlayer().sumHand(false);
-            if(lowVal > 21) {
-                this.channel.send("You went over!");
-                this.bust.push(this.curPlayer());
-                this.nextPlayer();
-            }
+        if(this.curPlayer().sumHand() > 21) {
+            this.channel.send("You went over!");
+            this.bust.push(this.curPlayer());
+            this.nextPlayer();
         }
     }
 
     nextPlayer() {
+        this.turn++;
+        if(this.checkWinners()){
+            return;
+        }
+        // check if one player left unbusted
         if(this.bust.length >= this.players.length - 1) {
             console.log("there is no next player");
             const winner = this.players.find(p => !this.bust.some(bp => bp.name == p.name));
-            this.channel.send("Winner is: " + winner.name);
+            let numNonwinners = this.players.reduce((acc, p) => p.name !== winner.name ? acc + 1 : acc, 0);
+            const winnings = winner.bet * (numNonwinners + 1); 
+            this.channel.send("Winner is: " + winner.name + " who bet: " + winner.bet.toString());
+            this.channel.send("You won: " + winnings.toString());
             this.finished = true;
+            console.log("finished from busting");
             return;
         }
-        this.turn++;
+        // if curplayer is bust, go to next player
         if(this.bust.some(p => p.name == this.curPlayer().name)) {
             this.nextPlayer();
         } else {
@@ -156,43 +170,181 @@ class Blackjack {
     stay() {
         this.nextPlayer();
     }
+
+    checkWinners() {
+        //check for everyone having finished
+        if(this.curPlayer() === undefined) {
+            // everyone is done playing
+            let winners = [];
+            let maxscore = 0;
+            for(let p of this.players) {
+                if(this.bust.some(bp => bp.name == p.name)) {
+                    continue; // this player is busted
+                }
+                if(p.sumHand() > maxscore) {
+                    maxscore = p.sumHand();
+                    winners = [p];
+                } else if(p.sumHand() == maxscore) {
+                    winners.push(p);
+                }
+            }
+            if(winners.length < 1) {
+                console.error("This should not happen ??");
+            }
+            this.players.forEach(p => p.points -= p.bet);
+            let numNonwinners = this.players.reduce((acc, p) => winners.includes(p) ? acc : acc + 1, 0);
+            if(winners.length == 1) {
+                const winner = winners[0];
+                this.channel.send("The winner is: " + winner.name + " who bet: " + winner.bet.toString());
+                this.channel.send("You won: " + (winner.bet*(numNonwinners + 1)).toString());
+                winner.points += winner.bet*(numNonwinners + 1);
+                console.log("Only one winner");
+            } else {
+                const winnersString = winners.reduce((acc, e) => acc + e.name + ": bet- " + e.bet.toString(), "");
+                this.channel.send("We have a tie! Winners: " + winnersString);
+                const winningsString = winners.reduce((acc, w) => acc + w.name + " won: " + (w.bet * (numNonwinners + 1)).toString() + " ", "");
+                this.channel.send(winningsString);
+                winners.forEach(w => w.points += w.bet * (numNonwinners + 1));
+                console.log("Tied winners");
+            }
+            this.finished = true;
+            console.log("finished from everyone playing");
+            return true;
+        }
+        return false;
+    }
+
+    newRound() {
+        let oldCards = [];
+        for(let p of this.players) {
+            oldCards = oldCards.concat(p.hand);
+            p.hand = [];
+        }
+        this.discard = this.discard.concat(oldCards);
+        this.turn = 0;
+        this.finished = false;
+        this.bust = [];
+        this.numBets = 0;
+    }
+
+    copyState(otherGame) {
+        this.channel = otherGame.channel;
+		this.players = [...otherGame.players];
+		this.deck = [...otherGame.deck]
+		this.discard = [...otherGame.discard];
+        this.turn = otherGame.turn;
+        this.bust = [...otherGame.bust];
+        this.finished = otherGame.finished;
+        this.numBets = otherGame.numBets;
+    }
+
+    isBetting() {
+        return this.numBets < this.players.length;
+    }
+
+    bet(amount) {
+        if(this.curPlayer().points < amount) {
+            this.channel.send(`You only have: ${this.curPlayer().points} points, bet smaller`);
+            return false;
+        }
+        this.curPlayer().bet = amount;
+        this.channel.send(this.curPlayer().name + " bet: " + amount.toString());
+        this.turn++;
+        this.numBets++;
+        if(this.turn == this.players.length) {
+            this.turn = 0;
+        }
+        this.sayTurn();
+        return true;
+    }
+
+    showPoints() {
+        this.players.forEach(p => this.channel.send(`${p.name}: ${p.points}`));
+    }
 }
 
-let prevGame = undefined;
-let curGame = undefined;
+let prevGame = new Blackjack(undefined, []);
+let curGame = new Blackjack(undefined, []);
 
 module.exports = {
     name: "blackjack",
-    usage: "blackjack (startingPoints) @<player1> @<player2> ... | hit | stay | restart",
+    usage: "blackjack (startingPoints) @<player1> @<player2> ... | hit | stay | restart | next",
     description: "starts blackjack game with mentioned players",
     action: (msg, cmdArgs) => {
-        let arg = cmdArgs[0];
+        let arg = cmdArgs[0].toLowerCase();
         
         if(arg == "hit") {
-            if(!curGame && msg.mentions.users.array().length < 1) {
+            if(curGame === undefined) {
                 msg.channel.send("No current game running");
                 return;
             }
             if(msg.author.username == curGame?.curPlayer().name) {
-                curGame?.hit();
+                if(curGame?.isBetting()) {
+                    msg.channel.send("All players must bet before hitting can start");
+                } else {
+                    curGame?.hit();
+                }
             } else {
                 msg.channel.send("Current active player is: " + curGame?.curPlayer().name);
             }
         } else if (arg == "stay"){
-            if(!curGame && msg.mentions.users.array().length < 1) {
+            if(curGame === undefined) {
                 msg.channel.send("No current game running");
                 return;
             }
             if(msg.author.username == curGame?.curPlayer().name) {
-                curGame?.stay();
+                if(curGame?.isBetting()) {
+                    msg.channel.send("All players must bet before game can start");
+                } else {
+                    curGame?.stay();
+                }
             } else {
                 msg.channel.send("Current active player is: " + curGame?.curPlayer().name);
+            }
+        } else if (arg == "bet") {
+            if(!curGame?.isBetting()) {
+                msg.channel.send("Current game already received bets");
+                return;
+            }
+            if(msg.author.username !== curGame?.curPlayer().name) {
+                msg.channel.send("Active player is: " + curGame?.curPlayer().name);
+                return;
+            }
+            let amount = parseInt(cmdArgs[1]);
+            if(!amount) {
+                msg.channel.send("Please bet an integer amount");
+            } else if (amount < 1) {
+                msg.channel.send("Please bet a positive integer amount");
+            } else {
+                curGame.bet(amount);
             }
         } else if (arg == "restart") {
             curGame = new Blackjack(msg.channel, prevGame.players);
             curGame.shuffle();
             curGame.dealInitial();
-            curGame.sayTurn();
+        } else if (arg == "next") {
+            if(curGame === undefined && prevGame.finished) {
+                console.log("going to next round");
+                curGame = new Blackjack(undefined, []);
+                curGame.copyState(prevGame);
+                curGame.newRound();
+                curGame.dealInitial();
+                console.log("curgame is now: ");
+                console.log(curGame);
+                console.log(curGame === undefined);
+            } else {
+                if(prevGame === undefined) {
+                    msg.channel.send("Start a game first");
+                } else {
+                    msg.channel.send("Current game is not finished");
+                }
+            }
+        } else if (arg == "deck") {
+            msg.channel.send("Game has " + curGame?.deck.length.toString() + " cards in deck");
+        } else if (arg == "cards") {
+            curGame?.showCards();
+        } else if (arg == "points") {
+            curGame?.showPoints();
         } else {
             const users = msg.mentions.users.array();
             if(users.length < 2) {
@@ -204,10 +356,10 @@ module.exports = {
             curGame = new Blackjack(msg.channel, players)
             curGame.shuffle();
             curGame.dealInitial();
-            curGame.sayTurn();
         }
         if(curGame?.finished) {
-            prevGame = curGame;
+            curGame.showPoints();
+            prevGame.copyState(curGame);
             curGame = undefined;
         }
     }
